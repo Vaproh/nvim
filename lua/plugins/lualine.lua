07f517f59f7d99b79fc5951047faa7780faffb44
Vaproh <contact@vaproh.xyz>
2024-11-16 19:06:11 +0530

    added new lualine theme inspired from evil_lualine template with onedark theme colors (custom)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,46 +2,136 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local colors = {
+             --> OneDark Theme
+            bg="#282c34",
+            fg="#abb2bf",
+            cyan="#56b6c2",
+            black="#181a1f",
+            gray="#5c6370",
+            magenta="#c678dd",
+            red="#e86671",
+            green="#98c379",
+            yellow="#e5c07b",
+            blue="#61afef",
+            orange="#d19a66",
+            black4="#3b3f4c", -- not part of one_dark, used `bg3`
+        }
+
+        local theme = {
+          normal = {
+            a = { fg = colors.bg, bg = colors.fg },
+            b = { fg = colors.fg, bg = colors.black4 },
+            c = { fg = colors.fg, bg = colors.bg },
+            z = { fg = colors.bg, bg = colors.fg },
+          },
+          insert = { a = { fg = colors.black, bg = colors.green } },
+          visual = { a = { fg = colors.black, bg = colors.blue } },
+          replace = { a = { fg = colors.black, bg = colors.magenta } },
+        }
+
+        local empty = require('lualine.component'):extend()
+        function empty:draw(default_highlight)
+          self.status = ''
+          self.applied_separator = ''
+          self:apply_highlights(default_highlight)
+          self:apply_section_separators()
+          return self.status
+        end
+
+        -- Put proper separators and gaps between components in sections
+        local function process_sections(sections)
+          for name, section in pairs(sections) do
+            local left = name:sub(9, 10) < 'x'
+            for pos = 1, name ~= 'lualine_z' and #section or #section - 1 do
+              table.insert(section, pos * 2, { empty, color = { fg = colors.gray, bg = colors.bg } })
+            end
+            for id, comp in ipairs(section) do
+              if type(comp) ~= 'table' then
+                comp = { comp }
+                section[id] = comp
+              end
+              comp.separator = left and { right = '' } or { left = '' }
+            end
+          end
+          return sections
+        end
+
+        local function search_result()
+          if vim.v.hlsearch == 0 then
+            return ''
+          end
+          local last_search = vim.fn.getreg('/')
+          if not last_search or last_search == '' then
+            return ''
+          end
+          local searchcount = vim.fn.searchcount { maxcount = 9999 }
+          return last_search .. '(' .. searchcount.current .. '/' .. searchcount.total .. ')'
+        end
+
+        local function modified()
+          if vim.bo.modified then
+            return '+'
+          elseif vim.bo.modifiable == false or vim.bo.readonly == true then
+            return '-'
+          end
+          return ''
+        end
+
         require('lualine').setup {
           options = {
-            icons_enabled = true,
-            theme = 'moonfly',
-            component_separators = { left = '\\', right = '/'},
-            section_separators = { left = '', right = ''},
-            disabled_filetypes = {
-              statusline = {},
-              winbar = {},
-            },
-            ignore_focus = {},
-            always_divide_middle = true,
-            always_show_tabline = true,
-            globalstatus = false,
-            refresh = {
-              statusline = 1000,
-              tabline = 1000,
-              winbar = 1000,
-            }
+            theme = theme, 
+            component_separators = '',
+            section_separators = { left = '', right = '' },
           },
-          sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_c = {'filename'},
-            lualine_x = {'encoding', 'fileformat', 'filetype'},
-            lualine_y = {'progress'},
-            lualine_z = {'location'}
+          sections = process_sections {
+            lualine_a = { 'mode' },
+            lualine_b = {
+              'branch',
+            },
+            lualine_c = {
+              'diff',
+              {
+                'diagnostics',
+                source = { 'nvim' },
+                sections = { 'error' },
+                diagnostics_color = { error = { bg = colors.red, fg = colors.white } },
+              },
+              {
+                'diagnostics',
+                source = { 'nvim' },
+                sections = { 'warn' },
+                diagnostics_color = { warn = { bg = colors.orange, fg = colors.white } },
+              },
+              { 'filename', file_status = false, path = 1 },
+              { modified, color = { bg = colors.red } },
+              {
+                '%w',
+                cond = function()
+                  return vim.wo.previewwindow
+                end,
+              },
+              {
+                '%r',
+                cond = function()
+                  return vim.bo.readonly
+                end,
+              },
+              {
+                '%q',
+                cond = function()
+                  return vim.bo.buftype == 'quickfix'
+                end,
+              },
+            },
+            lualine_x = {},
+            lualine_y = { search_result, 'filetype' },
+            lualine_z = { '%l:%c', '%p%%/%L' },
           },
           inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
+            lualine_c = { '%f %y %m' },
+            lualine_x = {},
           },
-          tabline = {},
-          winbar = {},
-          inactive_winbar = {},
-          extensions = {}
         }
     end
 }
